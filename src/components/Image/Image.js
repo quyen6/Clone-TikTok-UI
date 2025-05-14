6371b749964aae6e65fe78eb6a93570c9730c257
quyen6 <quyenthai.060603@gmail.com>
2025-05-14 21:40:23 +0700

    change-name-file

---
+++ src/components/Image/Image.js
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { useState, forwardRef } from "react";
+import images from "~/assets/image";
+import styles from "./Image.module.scss";
+import classNames from "classnames";
+
+const Image = forwardRef(
+  (
+    {
+      src,
+      alt,
+      className,
+      fallback: customFallback = images.noImage,
+      ...props
+    },
+    ref
+  ) => {
+    const [fallback, setFallback] = useState("");
+
+    const handleError = () => {
+      setFallback(customFallback);
+    };
+
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return (
+      <img
+        className={classNames(styles.wrapper, className)}
+        ref={ref}
+        src={fallback || src}
+        alt={alt}
+        {...props}
+        onError={handleError}
+      />
+    );
+  }
+);
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  fallback: PropTypes.string,
+};
+
+export default Image;
